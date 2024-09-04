b92c47371ea0b4ceb156bb24771129221a72e9d9
asii2020 <61282626+asii2020@users.noreply.github.com>
2024-09-04 12:01:49 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-// Your traffic source URL
-const trafficSourceUrl = "https://www.cpmrevenuegate.com/wqwsm6vbp6?key=2f1830b01b03351e6358375eb547156a";
+// URL of the video site
+const videoSiteUrl = "https://slfunzoneyt.blogspot.com/2024/09/aluthma-wela-video.html";
 
 // Function to handle the red button click
 function redirectAndStartTimer() {
@@ -7,7 +7,7 @@ function redirectAndStartTimer() {
     localStorage.setItem("redirectTime", new Date().getTime());
 
     // Redirect to the traffic source
-    window.location.href = trafficSourceUrl;
+    window.location.href = videoSiteUrl;
 }
 
 // Function to enable the green button after 25 seconds
@@ -18,7 +18,7 @@ function enableGreenButtonAfterTimer() {
     if (redirectTime) {
         const elapsedTime = new Date().getTime() - redirectTime;
 
-        if (elapsedTime >= 23000) {
+        if (elapsedTime >= 25000) {
             // If 25 seconds have already passed, enable the green button immediately
             greenButton.disabled = false;
         } else {
@@ -28,12 +28,17 @@ function enableGreenButtonAfterTimer() {
             }, 25000 - elapsedTime);
         }
     } else {
-        // If redirectTime is not set, enable the green button after 23 seconds
+        // If redirectTime is not set, enable the green button after 25 seconds
         setTimeout(() => {
             greenButton.disabled = false;
-        }, 23000);
+        }, 25000);
     }
 }
+
+// Add event listener for the green button click to redirect to the video site
+document.getElementById("greenButton").addEventListener("click", () => {
+    window.location.href = videoSiteUrl;
+});
 
 // Call this function when the page loads
 window.onload = enableGreenButtonAfterTimer;
